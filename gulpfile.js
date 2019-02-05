43c49bd6f8128ac599073cef16273dcdc8a646cb
mm-wang <mwang27@gmail.com>
2019-02-05 18:42:57 -0500

    adding vue-eslint-parser to throw linting errors at vue
    still need to fix the watch... why no watching anymore? :(

--- gulpfile.js
+++ gulpfile.js
@@ -16,58 +16,68 @@ const cleanup = require('rollup-plugin-cleanup');
 const babel = require('rollup-plugin-babel');
 
 let esLintRules = {
-    "eqeqeq": 0,
-    "no-use-before-define": 0
+	"eqeqeq": 0,
+	"no-use-before-define": 0
 };
 
 let esLintJs = {
-    parser: "babel-eslint",
-    rules: esLintRules,
-    globals: [
-        "$"
+	parser: "vue-eslint-parser",
+	extends: [
+		"plugin:vue/base"
+	],
+	parserOptions: {
+		parser: "babel-eslint",
+		sourceType: "module"
+	},
+	rules: esLintRules,
+	globals: [
+		"$"
+	],
+    plugins: [
+        "vue"
     ]
 };
 
 let baseDir = path.join(process.cwd(), "./");
 console.log("base directory: ", baseDir);
 let paths = {
-    browser: [baseDir + "browser/**/*.js"],
-    sass: [baseDir + "browser/css/*.scss"],
-    server: [baseDir + "server/**/*.js"]
+	browser: [baseDir + "browser/**/*.js", baseDir + "browser/**/*.vue"],
+	sass: [baseDir + "browser/css/*.scss"],
+	server: [baseDir + "server/**/*.js"]
 }
 
 let rollupOpts = {
-    es6Folder: baseDir + "browser/es6/**/*.js",
-    input: baseDir + "browser/es6/main.js",
-    output: {
-        format: "umd",
-        name: "main",
-        file: "./public/main.js",
-        indent: "  "
-    },
-    plugins: [
-        commonjs({
-            include: [
-                'node_modules/**'
-            ]
-        }),
-        vueplugin(),
-        cleanup(),
-        babel({
-            exclude: 'node_modules/**',
-            presets: [
-                ["@babel/preset-env", {
-                    modules: false
-                }]
-            ],
-            plugins: [
-                '@babel/plugin-external-helpers'
-            ],
-            runtimeHelpers: true,
-            externalHelpers: true
-        })
-    ],
-    sourceMap: true
+	es6Folder: baseDir + "browser/es6/**/*.js",
+	input: baseDir + "browser/es6/main.js",
+	output: {
+		format: "umd",
+		name: "main",
+		file: "./public/main.js",
+		indent: "  "
+	},
+	plugins: [
+		commonjs({
+			include: [
+				'node_modules/**'
+			]
+		}),
+		vueplugin(),
+		cleanup(),
+		babel({
+			exclude: 'node_modules/**',
+			presets: [
+				["@babel/preset-env", {
+					modules: false
+				}]
+			],
+			plugins: [
+				'@babel/plugin-external-helpers'
+			],
+			runtimeHelpers: true,
+			externalHelpers: true
+		})
+	],
+	sourceMap: true
 };
 
 // console.log(rollupOpts);
@@ -77,18 +87,18 @@ let rollupOpts = {
  */
 
 function reload(done) {
-    browserSync.reload();
-    done();
+	browserSync.reload();
+	done();
 }
 
 function serve(done) {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        },
-        open: false
-    });
-    done();
+	browserSync.init({
+		server: {
+			baseDir: './'
+		},
+		open: false
+	});
+	done();
 }
 
 
@@ -96,62 +106,62 @@ function serve(done) {
  Tasks
  */
 function prepSass() {
-    return gulp.src(paths.sass)
-        .pipe(sass())
-        .pipe(concat("style.css"))
-        .pipe(gulp.dest("./public"))
+	return gulp.src(paths.sass)
+		.pipe(sass())
+		.pipe(concat("style.css"))
+		.pipe(gulp.dest("./public"))
 }
 
 function prepJsBrowserSrc() {
-    return gulp.src(paths.browser)
-        .pipe(sourcemaps.init())
-        .pipe(concat("main.js"))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest("./public"));
+	return gulp.src(paths.browser)
+		.pipe(sourcemaps.init())
+		.pipe(concat("main.js"))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest("./public"));
 }
 
 async function jsRollup() {
-    const bundle = await rollup.rollup({
-        input: rollupOpts.input,
-        plugins: rollupOpts.plugins
-    });
-    let generated = await bundle.generate(rollupOpts.output);
-    let written = await bundle.write(rollupOpts.output);
-    return written;
+	const bundle = await rollup.rollup({
+		input: rollupOpts.input,
+		plugins: rollupOpts.plugins
+	});
+	let generated = await bundle.generate(rollupOpts.output);
+	let written = await bundle.write(rollupOpts.output);
+	return written;
 }
 
 function lintBrowserJs() {
-    function onError(err) {
-        notify.onError({
-            message: "Linting failed on browser, check gulp! Error: <%= error.message %>",
-        })(err);
-        this.emit('end');
-    };
+	function onError(err) {
+		notify.onError({
+			message: "Linting failed on browser, check gulp! Error: <%= error.message %>",
+		})(err);
+		this.emit('end');
+	};
 
-    return gulp.src(paths.browser)
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(eslint(esLintJs))
-        .pipe(eslint.format())
-        .pipe(eslint.failOnError());
+	return gulp.src(paths.browser)
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(eslint(esLintJs))
+		.pipe(eslint.format())
+		.pipe(eslint.failOnError());
 }
 
 function lintServerJs() {
-    function onError(err) {
-        notify.onError({
-            message: "Linting failed on server, check gulp! Error: <%= error.message %>",
-        })(err);
-        this.emit('end');
-    };
+	function onError(err) {
+		notify.onError({
+			message: "Linting failed on server, check gulp! Error: <%= error.message %>",
+		})(err);
+		this.emit('end');
+	};
 
-    return gulp.src(paths.server)
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(eslint(esLintJs))
-        .pipe(eslint.format())
-        .pipe(eslint.failOnError());
+	return gulp.src(paths.server)
+		.pipe(plumber({
+			errorHandler: onError
+		}))
+		.pipe(eslint(esLintJs))
+		.pipe(eslint.format())
+		.pipe(eslint.failOnError());
 }
 
 /*
@@ -171,9 +181,9 @@ buildWatch.description = "Default task: building and watching series";
 const buildRollup = gulp.series(jsRollup);
 
 module.exports = {
-    default: buildWatch,
-    watchSass: watchSass,
-    watchBrowserJs: watchBrowserJs,
-    rollup: buildRollup,
-    build: build
+	default: buildWatch,
+	watchSass: watchSass,
+	watchBrowserJs: watchBrowserJs,
+	rollup: buildRollup,
+	build: build
 };
