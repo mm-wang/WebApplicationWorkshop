9b3672424b2ae6fe98cb569c2e5948759e63a4d4
mm-wang <mwang27@gmail.com>
2019-02-11 10:22:57 -0500

    additional utilities

--- browser/es6/utilities/utilities.js
+++ browser/es6/utilities/utilities.js
@@ -1,4 +1,9 @@
-export const consoleTest = function() {
-    console.log("test!");
+export const formToJson = function(jqForm) {
+    console.log("in formToJson", jqForm, jqForm.serializeArray());
+	return jqForm.serializeArray().reduce(function(data, x) {
+        console.log(x);
+		data[x.name] = x.value;
+		return data;
+	}, {});
 }
-export default consoleTest;
+export default {};
