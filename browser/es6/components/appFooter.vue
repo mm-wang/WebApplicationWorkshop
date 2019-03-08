9fbf1df4867e73e6851e6f007cae21701751759d
mm-wang <mwang27@gmail.com>
2019-03-08 14:58:15 -0500

    I guess better to not have the svg anyway

--- browser/es6/components/appFooter.vue
+++ browser/es6/components/appFooter.vue
@@ -1,22 +1,22 @@
 <template>
 <footer>
-    <div class="sticky-bottom container-fluid bg-light">
-        <div class="float-right">
-            <a href="http://core.thorntontomasetti.com">
-                <img class="logo" src="https://s3-us-west-2.amazonaws.com/core-weblibrary/libraries/core-logo.svg" />
-            </a>&nbsp;&nbsp;&copy; {{Date.now() | year}}
-        </div>
+  <div class="sticky-bottom container-fluid bg-light">
+    <div class="float-right">
+      <a href="http://core.thorntontomasetti.com">
+        <img class="logo" src="https://s3-us-west-2.amazonaws.com/core-weblibrary/libraries/core-logo.png" />
+      </a>&nbsp;&nbsp;&copy; {{Date.now() | year}}
     </div>
+  </div>
 </footer>
 </template>
 <script>
 import moment from "moment";
 // console.log("THIS IS A TESTING THING");
 export default {
-    filters: {
-        year: function(date) {
-            return moment(date).format('YYYY');
-        }
+  filters: {
+    year: function(date) {
+      return moment(date).format('YYYY');
     }
+  }
 }
 </script>
