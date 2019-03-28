b000b71fbfc06557de514475bf10c373e6e7bbba
mm-wang <mwang27@gmail.com>
2019-03-28 16:53:25 -0400

    rhino compute commenting

--- server/external/RhinoCompute/index.js
+++ server/external/RhinoCompute/index.js
@@ -1,13 +1,23 @@
 let RhinoCompute = require("compute-rhino3d");
 RhinoCompute.authToken = "bearer " + process.env.RHINOCOMPUTE_TOKEN;
+/**
+ * Additional functions related to planes
+ * @type {Object}
+ */
 RhinoCompute.Plane = {
-		newPlane: function(pointO, vectorN, multiple = false) {
-			let url = "rhino/geometry/plane/new";
-			if (multiple) url = url + "?multiple=true"
-			let args = RhinoCompute.zipArgs(multiple, pointO, vectorN);
-			var promise = RhinoCompute.computeFetch(url, args);
-			return promise;
-		}
+	/**
+	 * Generating a new plane using a point and a vector normal
+	 * @param  {Point3d}  	pointO        A point, described using X, Y, and Z
+	 * @param  {Vector3d}  	vectorN       Vector3d referencing the orientation of the plane
+	 * @param  {Boolean} [multiple=false] Whether multiple are being added
+	 */
+	newPlane: function(pointO, vectorN, multiple = false) {
+		let url = "rhino/geometry/plane/new";
+		if (multiple) url = url + "?multiple=true"
+		let args = RhinoCompute.zipArgs(multiple, pointO, vectorN);
+		var promise = RhinoCompute.computeFetch(url, args);
+		return promise;
+	}
 };
 
 module.exports = RhinoCompute;
