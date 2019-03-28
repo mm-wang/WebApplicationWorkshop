77c2e4908952763edca586bb32787187c467d017
mm-wang <mwang27@gmail.com>
2019-03-28 16:59:52 -0400

    commenting routes

--- server/app/routes/_routes.js
+++ server/app/routes/_routes.js
@@ -6,8 +6,10 @@ module.exports = router;
 
 let viewsPath = path.join(__dirname, "..", "views/");
 
+// All route requests for the API calls to the server
 router.use("/api", require("server/app/routes/api/_api"));
 
+// Serving the application
 router.get("/*", function(req, res) {
-    res.sendFile(viewsPath + "index.html");
+	res.sendFile(viewsPath + "index.html");
 });
