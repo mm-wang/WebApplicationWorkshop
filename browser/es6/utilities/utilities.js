b6427d42b2e9cf30e3af48123f3655592154e463
mm-wang <mwang27@gmail.com>
2019-03-28 16:59:44 -0400

    don't need the forms json

--- browser/es6/utilities/utilities.js
+++
@@ -1,9 +0,0 @@
-export const formToJson = function(jqForm) {
-    console.log("in formToJson", jqForm, jqForm.serializeArray());
-	return jqForm.serializeArray().reduce(function(data, x) {
-        console.log(x);
-		data[x.name] = x.value;
-		return data;
-	}, {});
-}
-export default {};
